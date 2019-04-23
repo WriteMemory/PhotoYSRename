29c2e0cb3be9c095f0f6dc3d8e4c3cc7b96c9738
Doubing <re.bo.rn@foxmail.com>
2019-04-23 14:45:04 +0800

    Init

---
+++ PhotoRename/ConsoleApplicationRename/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplicationRename
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentPath = System.IO.Directory.GetCurrentDirectory();
+            GetDirectory(currentPath);
+            Console.WriteLine("文件重命名处理完毕。");
+            Console.ReadKey();
+        }
+        static void GetDirectory(string path)
+        {
+            SetDirectoryFilename(path);
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+                GetDirectory(dir);
+            }
+        }
+        static void SetDirectoryFilename(string dirPath)
+        {
+            //Console.WriteLine(dirPath);
+            string[] files = Directory.GetFiles(dirPath, "*_ys.*");
+            foreach (var item in files)
+            {
+                string afterName = item.Remove(item.LastIndexOf("_ys"), 3);
+                //Console.WriteLine(afterName);
+                Console.WriteLine(item);
+                File.Delete(afterName);
+                File.Move(item, afterName);
+            }
+        }
+    }
+}
